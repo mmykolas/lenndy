ef677cbcb5f18a38a41ad257fe3f9ffb29eb9465
mykolas <mykolas666@gmail.com>
2018-08-09 21:37:22 +0300

    Pradžia

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,16 @@
+<?php
+  namespace App\Controller;
+
+  use Symfony\Component\HttpFoundation\Response;
+  use Symfony\Component\Routing\Annotation\Route;
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+  class ArticleController {
+    /**
+     * @Route("/")
+     * @Method({"GET"})
+     */
+    public function index() {
+      return new Response('<html><body>HELLO!</body></html>');
+    }
+  }
