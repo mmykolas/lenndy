5b9d3532f1e2e14803402435125c343a02bd7b1b
mykolas <mykolas666@gmail.com>
2018-08-11 00:32:04 +0300

    Projektas įgauna vaizdą

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,13 +4,18 @@
   use Symfony\Component\HttpFoundation\Response;
   use Symfony\Component\Routing\Annotation\Route;
   use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+  use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-  class ArticleController {
+  class ArticleController extends Controller {
     /**
      * @Route("/")
      * @Method({"GET"})
      */
     public function index() {
-      return new Response('<html><body>HELLO!</body></html>');
+      // return new Response('<html><body>HELLO!</body></html>');
+
+      $articles = ['Article 1', 'Article 2'];
+
+      return $this->render('articles/index.html.twig', array('articles' => $articles));
     }
   }
