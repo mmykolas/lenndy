bcc437b4d43459f5d663e3f4b02aed4e60fd2e6b
mykolas <mykolas666@gmail.com>
2018-08-11 18:10:40 +0300

    Dar šiek tiek daugiau užduoties

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,6 +1,7 @@
 <?php
   namespace App\Controller;
 
+  use App\Entity\Article;
   use Symfony\Component\HttpFoundation\Response;
   use Symfony\Component\Routing\Annotation\Route;
   use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -17,5 +18,22 @@
       $articles = ['Article 1', 'Article 2'];
 
       return $this->render('articles/index.html.twig', array('articles' => $articles));
+    }
+
+    /**
+     * @Route("/article/save")
+     */
+    public function save() {
+      $entityManager = $this->getDoctrine()->getManager();
+
+      $article = new Article();
+      $article->setTitle('Article Two');
+      $article->setBody('This is the body for article two');
+
+      $entityManager->persist($article);
+
+      $entityManager->flush();
+
+      return new Response('Saved an article with the id of '.$article->getId());
     }
   }
